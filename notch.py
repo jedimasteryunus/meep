5c47cd75bc496a9ffc73a8136bad4f7fd3c82e19
Mikaeel Yunus <mikaeelyunus@MacBook-Pro-3.local>
2018-07-27 17:26:03 -0400

    Notch width loop *should* be in working order. I can't test it because I wrote it on a Mac. Will commit again if anything changes.

--- notch.py
+++ notch.py
@@ -6,171 +6,180 @@ from math import pi, tan, sqrt
 
 #os.system("rm -r notch-out/")
 
-#Waveguide Math
-a = 4 # length of cell
-h = 0.2 # height of Waveguide
-e = 0.4 # etch fraction [0 -> 1]
-w = 0.1 # width of the notch
-H = 10 * h #height of cell
+def notch(w): 
 
-h2 = h * (1 - e) #height of the etched region
-d = 1 #spacing of the waveguides
-posd = d / 2 #pos half d
-negd = -d / 2 #neg half d
+	#Waveguide Math
+	a = 4 # length of cell
+	h = 0.2 # height of Waveguide
+	e = 0.4 # etch fraction [0 -> 1]
+	H = 10 * h #height of cell
+	
+	h2 = h * (1 - e) #height of the etched region
+	d = 1 #spacing of the waveguides
+	posd = d / 2 #pos half d
+	negd = -d / 2 #neg half d
+	
+	ang = 80 #angle of the sidewall
+	angrad = ang * pi / 180
+	bottomoffset = h / tan(angrad)
+	c = bottomoffset / 2
+	r = sqrt(bottomoffset ** 2 + h ** 2)
+	
+	wavelength = 0.6372
+	fcen = 1 / wavelength
+	df = 0.05
+	
+	n_e = 2.2012 # refractive index inside waveguide (at wavelength 0.6372)
+	n_c = 1.4569 # refractive index outside waveguide (at wavelength 0.6372)
+	
+	#Waveguide Geometry
+	cell = mp.Vector3(a, H)
+	
+	default_material = mp.Medium(epsilon = n_c ** 2)
+	
+	geometry = [mp.Block(mp.Vector3(a + 2 * h, h),
+	            center = mp.Vector3(0,0),
+	            material = mp.Medium(epsilon = n_e ** 2)),
+	            mp.Block(mp.Vector3(w, e * h),
+	            center = mp.Vector3(0, h2 / 2),
+	            material = mp.Medium())]
+	
+	sources = [mp.EigenModeSource(mp.GaussianSource(frequency = fcen, fwidth = df),
+	                              size = mp.Vector3(0,H),
+	                              center = mp.Vector3(-0.75 * a / 2, 0))]
+	
+	pml_layers = [mp.PML(0.2)]
+	
+	resolution = 50
+	
+	sim = mp.Simulation(cell_size = cell,
+	                    boundary_layers = pml_layers,
+	                    geometry = geometry,
+	                    sources = sources,
+	                    resolution = resolution)
+	'''
+	#--------------------------------------------------
+	#FOR DISPLATYING THE GEOMETRY
+	
+	sim.run(until = 200)
+	
+	eps_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Dielectric)
+	plt.figure(dpi=100)
+	plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
+	#plt.axis('off')
+	plt.show()
+	
+	quit()
+	#----------------------------------------------------
+	'''
+	'''
+	#------------------------------------------------------
+	#FOR GENERATING THE ELECTRIC FIELD GIF
+	#Note: After running this program, write the following commands in Terminal:
+	    # $ source deactivate mp
+	    # $ cd notch-out/
+	    # $ python ../NotchIP.py
+	
+	sim.use_output_directory()
+	sim.run(mp.at_beginning(mp.output_epsilon),
+	        mp.to_appended("ez", mp.at_every(0.6, mp.output_efield_z)),
+	        until = 200)
+	#sim.run(mp.at_every(0.6 , mp.output_png(mp.Ez, "-Zc dkbluered")), until=200)
+	
+	quit()
+	#---------------------------------------------------------
+	'''
+	#---------------------------------------------------------
+	#FOR GENERATING THE TRANSMITTANCE SPECTRUM
+	
+	nfreq = 20  # number of frequencies at which to compute flux
+	
+	# reflected flux 1
+	refl_fr1 = mp.FluxRegion(center=mp.Vector3(-0.9 * a/2,0), size=mp.Vector3(0,3*h))
+	refl1 = sim.add_flux(fcen, df, nfreq, refl_fr1)
+	
+	#reflected flux 2
+	refl_fr2 = mp.FluxRegion(center=mp.Vector3(-0.6 * a/2,0), size=mp.Vector3(0,3*h))
+	refl2 = sim.add_flux(fcen, df, nfreq, refl_fr2)
+	
+	#transmitted flux
+	tran_fr = mp.FluxRegion(center = mp.Vector3(0.6 * a/2,0), size = mp.Vector3(0,3*h))
+	tran = sim.add_flux(fcen, df, nfreq, tran_fr)
+	
+	#flux loss above the waveguide
+	su_fr = mp.FluxRegion(center = mp.Vector3(0, 3*h), size = mp.Vector3(a,0))
+	su = sim.add_flux(fcen, df, nfreq, su_fr)
+	
+	#flux loss below the waveguide
+	sd_fr = mp.FluxRegion(center = mp.Vector3(0, -3*h), size = mp.Vector3(a,0))
+	sd = sim.add_flux(fcen, df, nfreq, sd_fr)
+	
+	pt = mp.Vector3(9.75,0)
+	
+	sim.run(until_after_sources=mp.stop_when_fields_decayed(50,mp.Ez,pt,1e-3))
+	
+	# save incident power for reflection planes
+	straight_refl1_flux = mp.get_fluxes(refl1)
+	straight_refl2_flux = mp.get_fluxes(refl2)
+	
+	# save incident power for transmission plane
+	straight_tran_flux = mp.get_fluxes(tran)
+	
+	# save incident power for loss planes
+	straight_su_flux = mp.get_fluxes(su)
+	straight_sd_flux = mp.get_fluxes(sd)
+	
+	wl = [] #list of wavelengths
+	
+	refl1_flux = mp.get_fluxes(refl1)
+	refl2_flux = mp.get_fluxes(refl2)
+	tran_flux = mp.get_fluxes(tran)
+	su_flux = mp.get_fluxes(su)
+	sd_flux = mp.get_fluxes(sd)
+	
+	flux_freqs = mp.get_flux_freqs(refl1)
+	
+	for i in range(nfreq):
+	    wl = np.append(wl, 1/flux_freqs[i])
+	
+	for ind, elt in enumerate(wl):
+	    #print(round(elt, 4))
+	    if round(elt, 3) == 0.637:
+	        #print("ALERT: MATCH FOUND")
+	        index = ind
+	
+	R = -refl1_flux[index] / (refl2_flux[index] - refl1_flux[index])
+	T = tran_flux[index] / (refl2_flux[index] - refl1_flux[index])
+	S = (refl2_flux[index] - tran_flux[index]) / (refl2_flux[index] - refl1_flux[index])
+	Su = su_flux[index] / (refl2_flux[index] - refl1_flux[index])
+	Sd = -sd_flux[index] / (refl2_flux[index] - refl1_flux[index])
+	
+	norm_Su = S * Su / (Su + Sd)
+	
+	NET = round((R + T + S) * 100, 0)
+	if NET > 100.0:
+	    NET = 100.0
+	
+	NET_LOSS = round((Su + Sd) / S * 100, 0)
+	if NET_LOSS > 100.0:
+	    NET_LOSS = 100.0
+	
+	print("---------------------------------------------------")
+	print("Notch Width: ", w * 1000, "nanometers")
+	print("Reflection Percentage: ", R * 100)
+	print("Transmission Percentage: ", T * 100)
+	print("Total Loss Percentage: ", S * 100)
+	print("Percentage of Light Accounted For: ", NET)
+	print("Upper Loss Percentage: ", Su * 100)
+	print("Lower Loss Percentage: ", Sd * 100)
+	print("Percentage of Total Loss Accounted For: ", NET_LOSS)
+	print("Normalized Upper Loss Percentage: ", norm_Su * 100)
+	print("---------------------------------------------------")
 
-ang = 80 #angle of the sidewall
-angrad = ang * pi / 180
-bottomoffset = h / tan(angrad)
-c = bottomoffset / 2
-r = sqrt(bottomoffset ** 2 + h ** 2)
+	quit()
+	#-------------------------------------------------------------
 
-wavelength = 0.6372
-fcen = 1 / wavelength
-df = 0.05
+for notch_width in range(0.04, 0.30, 0.02): 
+	notch(notch_width)
 
-n_e = 2.2012 # refractive index inside waveguide (at wavelength 0.6372)
-n_c = 1.4569 # refractive index outside waveguide (at wavelength 0.6372)
 
-#Waveguide Geometry
-cell = mp.Vector3(a, H)
-
-default_material = mp.Medium(epsilon = n_c ** 2)
-
-geometry = [mp.Block(mp.Vector3(a + 2 * h, h),
-            center = mp.Vector3(0,0),
-            material = mp.Medium(epsilon = n_e ** 2)),
-            mp.Block(mp.Vector3(w, e * h),
-            center = mp.Vector3(0, h2 / 2),
-            material = mp.Medium())]
-
-sources = [mp.EigenModeSource(mp.GaussianSource(frequency = fcen, fwidth = df),
-                              size = mp.Vector3(0,H),
-                              center = mp.Vector3(-0.75 * a / 2, 0))]
-
-pml_layers = [mp.PML(0.2)]
-
-resolution = 50
-
-sim = mp.Simulation(cell_size = cell,
-                    boundary_layers = pml_layers,
-                    geometry = geometry,
-                    sources = sources,
-                    resolution = resolution)
-'''
-#--------------------------------------------------
-#FOR DISPLATYING THE GEOMETRY
-
-sim.run(until = 200)
-
-eps_data = sim.get_array(center=mp.Vector3(), size=cell, component=mp.Dielectric)
-plt.figure(dpi=100)
-plt.imshow(eps_data.transpose(), interpolation='spline36', cmap='binary')
-#plt.axis('off')
-plt.show()
-
-quit()
-#----------------------------------------------------
-'''
-'''
-#------------------------------------------------------
-#FOR GENERATING THE ELECTRIC FIELD GIF
-#Note: After running this program, write the following commands in Terminal:
-    # $ source deactivate mp
-    # $ cd notch-out/
-    # $ python ../NotchIP.py
-
-sim.use_output_directory()
-sim.run(mp.at_beginning(mp.output_epsilon),
-        mp.to_appended("ez", mp.at_every(0.6, mp.output_efield_z)),
-        until = 200)
-#sim.run(mp.at_every(0.6 , mp.output_png(mp.Ez, "-Zc dkbluered")), until=200)
-
-quit()
-#---------------------------------------------------------
-'''
-#---------------------------------------------------------
-#FOR GENERATING THE TRANSMITTANCE SPECTRUM
-
-nfreq = 20  # number of frequencies at which to compute flux
-
-# reflected flux 1
-refl_fr1 = mp.FluxRegion(center=mp.Vector3(-0.9 * a/2,0), size=mp.Vector3(0,3*h))
-refl1 = sim.add_flux(fcen, df, nfreq, refl_fr1)
-
-#reflected flux 2
-refl_fr2 = mp.FluxRegion(center=mp.Vector3(-0.6 * a/2,0), size=mp.Vector3(0,3*h))
-refl2 = sim.add_flux(fcen, df, nfreq, refl_fr2)
-
-#transmitted flux
-tran_fr = mp.FluxRegion(center = mp.Vector3(0.6 * a/2,0), size = mp.Vector3(0,3*h))
-tran = sim.add_flux(fcen, df, nfreq, tran_fr)
-
-#flux loss above the waveguide
-su_fr = mp.FluxRegion(center = mp.Vector3(0, 3*h), size = mp.Vector3(a,0))
-su = sim.add_flux(fcen, df, nfreq, su_fr)
-
-#flux loss below the waveguide
-sd_fr = mp.FluxRegion(center = mp.Vector3(0, -3*h), size = mp.Vector3(a,0))
-sd = sim.add_flux(fcen, df, nfreq, sd_fr)
-
-pt = mp.Vector3(9.75,0)
-
-sim.run(until_after_sources=mp.stop_when_fields_decayed(50,mp.Ez,pt,1e-3))
-
-# save incident power for reflection planes
-straight_refl1_flux = mp.get_fluxes(refl1)
-straight_refl2_flux = mp.get_fluxes(refl2)
-
-# save incident power for transmission plane
-straight_tran_flux = mp.get_fluxes(tran)
-
-# save incident power for loss planes
-straight_su_flux = mp.get_fluxes(su)
-straight_sd_flux = mp.get_fluxes(sd)
-
-wl = [] #list of wavelengths
-
-refl1_flux = mp.get_fluxes(refl1)
-refl2_flux = mp.get_fluxes(refl2)
-tran_flux = mp.get_fluxes(tran)
-su_flux = mp.get_fluxes(su)
-sd_flux = mp.get_fluxes(sd)
-
-flux_freqs = mp.get_flux_freqs(refl1)
-
-for i in range(nfreq):
-    wl = np.append(wl, 1/flux_freqs[i])
-
-for ind, elt in enumerate(wl):
-    #print(round(elt, 4))
-    if round(elt, 3) == 0.637:
-        #print("ALERT: MATCH FOUND")
-        index = ind
-
-R = -refl1_flux[index] / (refl2_flux[index] - refl1_flux[index])
-T = tran_flux[index] / (refl2_flux[index] - refl1_flux[index])
-S = (refl2_flux[index] - tran_flux[index]) / (refl2_flux[index] - refl1_flux[index])
-Su = su_flux[index] / (refl2_flux[index] - refl1_flux[index])
-Sd = -sd_flux[index] / (refl2_flux[index] - refl1_flux[index])
-
-norm_Su = S * Su / (Su + Sd)
-
-NET = round((R + T + S) * 100, 0)
-if NET > 100.0:
-    NET = 100.0
-
-NET_LOSS = round((Su + Sd) / S * 100, 0)
-if NET_LOSS > 100.0:
-    NET_LOSS = 100.0
-
-print("Reflection Percentage: ", R * 100)
-print("Transmission Percentage: ", T * 100)
-print("Total Loss Percentage: ", S * 100)
-print("Percentage of Light Accounted For: ", NET)
-print("Upper Loss Percentage: ", Su * 100)
-print("Lower Loss Percentage: ", Sd * 100)
-print("Percentage of Total Loss Accounted For: ", NET_LOSS)
-print("Normalized Upper Loss Percentage: ", norm_Su * 100)
-
-quit()
-#-------------------------------------------------------------
