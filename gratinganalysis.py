f016e48070e701519efcb7247d69207d961e14d0
Mikaeel Yunus <mikaeelyunus@RLE-EECS-MTL-DHCP-30-2.MIT.EDU>
2018-08-10 18:25:05 -0400

    Continuing debugging and dcode evelopment for gratinganalysis.py

--- gratinganalysis.py
+++ gratinganalysis.py
@@ -32,6 +32,11 @@ def reflection(width):
 	return r
 
 def scatter(width): 
+    index = widthToIndex(width)
+    s = sqrt(data[-1][index])
+    return s
+
+def scatter_matrix(width): 
 	t = transmission(width)
 	r = reflection(width)
 
@@ -39,7 +44,7 @@ def scatter(width):
                         [ r/t,   (r*r + t*t)/t   ]  ], 
                         dtype=complex)
 
-def propagate(length): 
+def propagate_matrix(length): 
     return np.matrix([  [ np.exp(-pi*2j*neff*length/wavelength),              0                     ],
                         [ 0,                                 np.exp(pi*2j*neff*length/wavelength)   ]  ], 
                         dtype=complex)
@@ -51,11 +56,11 @@ def getAmplitudes(widths, lengths):
     a = np.zeros((2, 2*N), dtype=complex)                              # N times [[ a  b  ]; [ b' a' ]].
 
     a[0, 2*N-1] = 1                                     # Set the initial vector (b = 1, a' = 0).
-    a[:, 2*N-2] = a[:, 2*N-1] * scatter(widths[N-1])   # And find a, b' for the first grate.
+    a[:, 2*N-2] = a[:, 2*N-1] * scatter_matrix(widths[N-1])   # And find a, b' for the first grate.
 
     for ii in range(N-1):                               # Now do this for the rest of the grates.
-        a[:, 2*ii+1] = a[:, 2*ii+2] * propagate(lengths[ii])
-        a[:, 2*ii] = a[:, 2*ii+1]  * scatter(widths[ii])
+        a[:, 2*ii+1] = a[:, 2*ii+2] * propagate_matrix(lengths[ii])
+        a[:, 2*ii] = a[:, 2*ii+1]  * scatter_matrix(widths[ii])
 
     return a 
 
@@ -64,12 +69,22 @@ def E(x,W):
     return np.exp(-x**2 / W**2)
 
 def getOverlap(widths, lengths, amplitudes, W, grating_length, num_notches):
+    
+    s = np.zeros(num_notches, dtype = complex)
+
+    for i in range(0, num_notches): 
+        print("Scatter:", scatter(widths[i]))
+        print("Amplitudes:", amplitudes[2*i,0] + amplitudes[2*i+1,1])
+
+    for i in range(0, num_notches): 
+        s[i] = scatter(widths[i])*(amplitudes[2*i,0] + amplitudes[2*i+1,1])
+
     final_gamma = 0
     for X in range(0, grating_length + 100, 100):
         gamma = 0
         for i in range(0, num_notches): 
-            gamma += amplitudes[i] * E(lengths[i] - X, W)
-        gamma = gamma**2 
+            gamma += s[i] * E(lengths[i] - X, W)
+        gamma = np.abs(gamma)**2
         final_gamma = max(gamma, final_gamma)
     return final_gamma
 
