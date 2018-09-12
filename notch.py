097fcba0c20c19f859e5f875b1979b63c05e0e8e
Mikaeel Yunus <myunus@mit.edu>
2018-09-11 22:44:51 -0400

    Put a snippet of code at the end to make sure the reflected/transmitted power stays the same when mode separation is factored into the code.

--- notch.py
+++ notch.py
@@ -568,6 +568,14 @@ writeme(t11s, p1);
 writeme(sd1s, p1);
 writeme(su1s, p1);
 
+'''
+for i in range(len(Rs)): 
+	print("Reflected Power Without Mode Separation: ", Rs[i])
+	print("Reflected Power With Mode Separation: ", r00s[i]**2 + r01s[i]**2)
+	print("Transmitted Power Without Mode Separation: ", Ts[i])
+	print("Transmitted Power With Mode Separation: ", t00s[i]**2 + t01s[i]**2)
+'''
+
 f2.write("%s \n" % (n_eff_funds))	# 31
 f2.write("%s \n" % (n_eff_firsts))	# 32
 
